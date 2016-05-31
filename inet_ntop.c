ba61148d2eec0d4347fcbefe9b94eff34e1e4240
duper <super@blackcatz.org>
2016-05-31 04:14:44 -0400

    Adding arpaname from ISC's BIND source tree and the inet_pton example from the Linux Programmer's Manual

--- inet_ntop.c
+++ inet_ntop.c
@@ -13,21 +13,25 @@ static void vexit(const char *fn) {
   exit(EXIT_FAILURE);
 }
 
-static void usage(const char *const a0) {
+static void usage(const char *restrict const a0) {
   fprintf(stderr, "usage: %s BASE10IP4\n", a0);
   fputs(" BASE10IP4  numeric IPv4 address encoded in decimal\n", stderr); 
 
   exit(EX_USAGE);
 }
 
+typedef struct sockaddr_in struct sockaddr_in4;
+
 int main(int ac, char *av[], char *ev[]) {
-  static struct sockaddr_in sa = { '\0' };
-  char ia[INET_ADDRSTRLEN] = { 0 };
+  struct sockaddr_in4 in4 = { 0x00, };
+  struct sockaddr_in6 in6 = { 0x00, };
+  signed char ip4[INET_ADDRSTRLEN] = { '\0', }, ip6[INET6_ADDRSTRLEN] = { '\0', }, *end;
+
 
   if(ac < 2)
     usage(*av);
 
-  sa.sin_addr.s_addr = atoi(av[1]);
+  const signed long int dec = strtol(av[1], &env, 0xA);
  
   if(!inet_ntop(PF_INET, &(sa.sin_addr), ia, sizeof ia))
     vexit("inet_ntop");
